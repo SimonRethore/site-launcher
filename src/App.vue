2db6b84bc22b2a32dfff9bce211b3c91624d1c7d
SimonRethore <srethore.pro@gmail.com>
2023-03-29 18:34:33 +0200

    Implement layout's header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
   <router-view/>
 </template>
+
+<style lang="scss">
+body{
+  margin: 0;
+}
+
+@font-face {
+  font-family: "LuckiestGuy";
+  src: url("@/assets/fonts/LuckiestGuy-Regular.ttf") format("truetype");
+  font-weight: normal;
+  font-style: normal;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div id="home-view">
+    <div class="header">
+      <img src="@/assets/images/logo.png">
+      <h1>SITE LAUNCHER</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +14,40 @@ export default {
   components: {}
 }
 </script>
+
+<style scoped lang="scss">
+.header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 112px;
+  background-color: #FAF3E0;
+
+  img{
+    height: 70px;
+    width: 70px;
+  }
+
+  h1{
+    margin: 5px 0 0 10px;
+    font-family: 'LuckiestGuy';
+    font-size: 28px;
+    letter-spacing: 2px;
+  }
+}
+
+@media (min-width: 768px) {
+  .header{
+    height: 150px;
+
+    img{
+      height: 100px;
+      width: 100px;
+    }
+
+    h1{
+      font-size: 40px;
+    }
+  }
+}
+</style>
